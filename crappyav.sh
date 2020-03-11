2f5481aa3a813ba2f7ce13cdcb452c5bfe39788c
Will Bollock <wbollock@gmail.com>
2020-03-11 19:04:24 -0400

    init

---
+++ crappyav.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#================================================================
+#% SYNOPSIS
+#+    CrappyAV
+#%
+#% DESCRIPTION
+#%    A terrible CLI AV. Grabs MD5 hashes of virus files 
+#%    and lets the user scan individual files to see if 
+#%    they match against known virus hashes.
+#%
+#% USAGE
+#%    ${SCRIPT_NAME} -o DEFAULT arg1 arg2
+#%
+#================================================================
