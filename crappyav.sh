99cfcd7797416ee702b99c9de6cc6d25df519b60
Will Bollock <wbollock@gmail.com>
2020-03-12 21:24:11 -0400

    formatting and shit

--- crappyav.sh
+++ crappyav.sh
@@ -6,7 +6,7 @@
 #%
 #% DESCRIPTION
 #%    A terrible CLI AV. Grabs MD5 hashes of virus files 
-#%    and lets the user scan individual files to see if 
+#%    and lets the user scan files to see if 
 #%    they match against known virus hashes.
 #%
 #% USAGE
@@ -20,16 +20,15 @@
 # Prerequisites:
 # for zsh: zmodload zsh/mapfile
 # for bash: mapfile should work in any version >4.0
-
-# for developer: https://github.com/phw/peek
+# for cool gifs: https://github.com/phw/peek
 
 # Potential Problems: 
 # array with 131072 values. Maybe kenny can help on that
-# how to handle downloading all the files?
 # how to get a goddamn test file
 
-# TODO: log file. maybe log filename, hash of file, if file found, hashes tested,etc maybe ask user if they want to log?
-# this will be a status page on a webserver
+# TODO CHECKLIST:
+# Hash function checking needs a lot of work
+# status page shit
 
 
 # CONFIG VALUES
@@ -62,12 +61,8 @@ YELLOW='\033[0;33m'
 
 
 
-
-# 1. Download a metric shit ton of MD5 hashes
+# Download a metric shit ton of MD5 hashes
 # or, well, one
-
-
-
 downloadHashes(){
 
     echo -e "${RED}Please note downloading all of these hashes requires 1.1GB of disk space and bandwith.${NC}"
@@ -79,7 +74,7 @@ downloadHashes(){
     case $hashChoice in
     # N is bigger letter in prompt and therefore default, thats why "" is added to switch statement
 		y|Y) allHashes ;;
-		n|N|"") oneHash ;;
+		n|N|"") oneHashDL ;;
 		*) echo -e "${RED}Error...${NC}" && sleep .5
 	esac
 
@@ -112,7 +107,7 @@ allHashes(){
 }
 
 
-oneHash(){
+oneHashDL(){
     
     echo "One hash file will be downloaded."
     sleep 1
@@ -122,85 +117,60 @@ oneHash(){
         wget -O "$hashDir"/"$hashfile" https://virusshare.com/hashes/VirusShare_00000.md5
     fi
 
-
     # Strip hashfile of the top header
     # thankfully all header lines started with #
-
     if [ ! -f "$hashDir"/"$hashfileFixed" ]; then
-    # same deal, if hash file hasn't already been fixed don't do it again
+    
         sed '/^#/ d' < "$hashDir"/"$hashfile" > "$hashDir"/"$hashfileFixed"
         rm -f "$hashDir"/"$hashfile"
         # clear up old hash file to save space
     fi
-    # rmeove hashfile anyway
+    
     rm -f "$hashDir"/"$hashfile"
-    # this should be fine for just the single hash.
 
 }
 
-
-
-
-
-# DEBUG: remember to comment these out
-# test number of array elements
-#echo ${#hashArray[*]}
-
-# test a random array element
-#echo "${hashArray[1337]}"
-
-# damn this worked great
-# also worked in bash
-
-
-
-# 3. Take user input on what file they want to see if it's malicious
+#Take user input on what file they want to see if it's malicious
 hashCheck(){
     # this needs to work for one hash file as well as 376
 
-# TODO:will need to check on amount on hash files downloaded...
-# that will help know what file/files to compare against. maybe check to see amount of file in hashDir?
+    # TODO:will need to check on amount on hash files downloaded...
+    # that will help know what file/files to compare against. maybe check to see amount of file in hashDir?
 
-mapfile -t hashArray < "$hashDir"/"$hashfileFixed"
-# above only works for 
-
-
-echo -e "${BLUE}Hi user. Tell me what file you think is malware:${NC}"
-# example: /home/wbollock/class/CrappyAV/testvirus.txt
-read -r scaryVirus
+    mapfile -t hashArray < "$hashDir"/"$hashfileFixed"
+    # above only works for 
 
 
-# 4. Calculate MD5 hash of user file (hash should match downloaded ones)
-# md5sum program used for this. should be installed on most *nix
-# md5sum output: ed0335c6becd00a2276481bb7561b743  testvirus.txt
-# I wish it would only print the hash.. man page wasn't helpful
-# https://unix.stackexchange.com/questions/65932/how-to-get-the-first-word-of-a-string
-fileHash=$(md5sum "$scaryVirus"  | head -n1 | awk '{print $1;}')
-
-echo "Thanks for that. Doing some really smart algorithm..."
-sleep 1
-
-echo "File hash is: $fileHash"
-# damn this works too. on a roll. hash is 32 characters
+    echo -e "${BLUE}Hi user. Tell me what file you think is malware:${NC}"
+    # example: /home/wbollock/class/CrappyAV/testvirus.txt
+    read -r scaryVirus
 
 
+    # 4. Calculate MD5 hash of user file (hash should match downloaded ones)
+    # md5sum program used for this. should be installed on most *nix
+    # md5sum output: ed0335c6becd00a2276481bb7561b743  testvirus.txt
+    # I wish it would only print the hash.. man page wasn't helpful
+    # https://unix.stackexchange.com/questions/65932/how-to-get-the-first-word-of-a-string
+    fileHash=$(md5sum "$scaryVirus"  | head -n1 | awk '{print $1;}')
 
+    echo "Thanks for that. Doing some really smart algorithm..."
+    sleep 1
 
+    echo "File hash is: $fileHash"
+    # damn this works too. on a roll. hash is 32 characters
+    # 5. Compare hash of user file to my big list of hashes
 
+    for hash in "${hashArray[@]}"
+    do
+    # compare fileHash to my big ass list
+    if [ "$hash" == "$fileHash" ]; then
+        echo -e "${RED}Match found! This wasn't supposed to happen. Wipe your drive."
+    fi
+    echo "$hash"
+    # TODO: It'd be cool if this made a rainbow
+    done
 
-# 5. Compare hash of user file to my big list of hashes
-
-for hash in "${hashArray[@]}"
-do
- # compare fileHash to my big ass list
- if [ "$hash" == "$fileHash" ]; then
-    echo -e "${RED}Match found! This wasn't supposed to happen. Wipe your drive."
- fi
- echo "$hash"
- # TODO: It'd be cool if this made a rainbow
-done
-
-echo -e "${RED}Our advanced blockchain neural-network AI didn't find anything wrong with the file. Proceed as normal!${NC}"
+    echo -e "${RED}Our advanced blockchain neural-network AI didn't find anything wrong with the file. Proceed as normal!${NC}"
 
 }
 
@@ -212,30 +182,27 @@ updateStatusPage(){
 # Amount of hashes downloaded
 # Hash of last file checked
 # Number of exploits found all time
+
+# probably best to call a second script with parameters
 echo "updateStatusPage Placeholder"
 sleep 2
 }
 
-# -----------------------------------
-# Main logic - infinite loop
-# ------------------------------------
-deleteHashes(){
 
-    echo -e "${RED}Alright, we will clear your hashes.${NC}"
-    echo ""
-    # don't be a dummy and parse ls. echo works too
+deleteHashes(){
+    # don't be a dummy and parse ls. find is much bette
     echo  -e "The hash directory has a size of ${BLUE}$(du -h $hashDir | head -n1 | awk '{print $1;}')${NC} and ${BLUE}$(find $hashDir -type f | wc -l)${NC} file(s)."
     echo ""
     sleep 1
-    echo -e "${RED}Really delete?${NC} [Y/n]"
+    echo -e "${RED}Really delete?${NC} [y/N]"
     read -r delChoice
     case $delChoice in
     # N is bigger letter in prompt and therefore default, thats why "" is added to switch statement
-		y|Y|"") rm -rf "${hashDir:?}/"* ;;
+		y|Y) rm -rf "${hashDir:?}/"* ;;
         # avoid deleting entire filesystem with question mark
         # https://github.com/koalaman/shellcheck/wiki/SC2115
         # if null it won't rm -rf /*
-		n|N) echo "No files will be deleted."
+		n|N|"") echo "No files will be deleted."
         sleep 1 ;;
 		*) echo -e "${RED}Error...${NC}" && sleep .5
 	esac
@@ -245,7 +212,6 @@ deleteHashes(){
 # Menu Function
 # Display list of options, start at top of decision tree
 # Inspiration from https://bash.cyberciti.biz/guide/Menu_driven_scripts
-
 show_menus() {
 	clear
 	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"	
@@ -259,7 +225,8 @@ show_menus() {
     echo "4) Delete all hash files from system"
     echo "5) Exit"
 }
-# Read options. Call another function from 1 or 2.
+
+# Read options. Call another function from choices
 read_options(){
 	local choice
     echo ""
@@ -275,6 +242,10 @@ read_options(){
 	esac
 }
 
+
+# -----------------------------------
+# Main logic - infinite loop
+# ------------------------------------
 while true
 do
 	show_menus
